45b909a705b22abbef12db6b6eb8a86547246e69
medarcho4 <adammazhidov7@gmail.com>
2024-07-18 19:17:15 +0300

    homework6.py

--- homework6.py
+++ homework6.py
@@ -1,10 +1,9 @@
 my_dict={"Real Madrid": 15, "barcelona": 5}
 print(my_dict)
 print(my_dict["Real Madrid"])
-my_dict["Manchester City"]=1
-print(my_dict)
+print(my_dict.get("Manchester City")
 my_dict.update({"Bayern Munich": 6, "Milan": 7})
-x=my_dict.pop("Manchester City", "barcelona")
+x=my_dict.pop("barcelona")
 print(x)
 
 my_set={1,1,"a","a"}
